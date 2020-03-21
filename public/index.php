49ceec481f197a985699e3159080c0fc1460c74c
springwind2006 <spring.wind2006@163.com>
2020-03-21 15:38:28 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-define('ROOT_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
-include dirname(__FILE__).'/../kernel/base.php';
-!defined('TEMPLATE_REPARSE') && define('TEMPLATE_REPARSE',APP_DEBUG); //模版缓存
+define('BASE_PATH', dirname(__DIR__).DIRECTORY_SEPARATOR);
+include BASE_PATH.'kernel/base.php';
+define('TEMPLATE_REPARSE', APP_DEBUG); //模版缓存
 Loader::app();
